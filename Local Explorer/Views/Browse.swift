a32b9521ccd7a96483e2a3bbcbab825484173d22
tonyliu2004 <tony.liu404@gmail.com>
2025-10-12 13:57:12 -0700

    Implemented Google places API and displays.,

---
+++ Local Explorer/Views/Browse.swift
@@ -0,0 +1,19 @@
+//
+//  BrowseV.swift
+//  Local Explorer
+//
+//  Created by Tony Liu on 10/12/25.
+//
+
+import SwiftUI
+
+struct BrowseV: View {
+    var body: some View {
+        VStack {
+            Text("BrowseV Page")
+                .font(.largeTitle)
+            Spacer()
+        }
+        .padding()
+    }
+}
